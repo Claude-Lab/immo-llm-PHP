bcff7b09b207602130532c2de114b09e4cd0a778
Claude Lusseau <claude.lusseau@yahoo.fr>
2021-08-28 20:55:36 +0200

    dechets

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
